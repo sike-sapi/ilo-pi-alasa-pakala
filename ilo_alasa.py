f6781372ee5d8ad9c722e69c925a952ee8f7f3cd
sike Sapi <sike.sapi@hexclimbers.com>
2025-07-10 22:49:43 +0100

    ilo li kama e lipu pana

--- ilo_alasa.py
+++ ilo_alasa.py
@@ -12,39 +12,38 @@ async def hello(ctx):
 
 @bot.slash_command(guild_ids=[1372566140110508143])
 async def submit(ctx):
-    await ctx.respond("Submit!")
-    # modal = SubmitModal(title="Sentence submission")
-    #
-    # # Send the prompt for the sentence
-    # await ctx.send_modal(modal)
-    # await modal.wait()
-    #
-    # submission = modal.children[0].value
-    # channel = self.bot.get_channel(1392972779971678452)
-    # resp = await channel.send(
-    #     content=f"{ctx.user.mention} li pana e toki ni:\n"
-    #     f"> {submission}\n",
-    # )
+    modal = SubmitModal(title="Sentence submission")
 
-# class SubmitModal(discord.ui.Modal):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#
-#         self.add_item(
-#             discord.ui.InputText(
-#                 label="Your sentence here!",
-#                 max_length=200,
-#                 style=discord.InputTextStyle.long,
-#                 required=True,
-#             )
-#         )
-#
-#     async def callback(self, interaction: discord.Interaction):
-#         await interaction.response.send_message(
-#             content="mi pana e toki sina tawa lawa ma! lawa li wile lukin e toki sina. o awen.\n"
-#             "I submitted your sentence to the overlords! Please wait for approval.",
-#             ephemeral=True,
-#         )
-#         self.stop()
+    # Send the prompt for the sentence
+    await ctx.send_modal(modal)
+    await modal.wait()
+
+    submission = modal.children[0].value
+    channel = bot.get_channel(1392972779971678452)
+    resp = await channel.send(
+        content=f"{ctx.user.mention} li pana e toki ni:\n"
+        f"> {submission}\n",
+    )
+
+class SubmitModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.add_item(
+            discord.ui.InputText(
+                label="Your sentence here!",
+                max_length=200,
+                style=discord.InputTextStyle.long,
+                required=True,
+            )
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            content="mi pana e toki sina tawa lawa ma! lawa li wile lukin e toki sina. o awen.\n"
+            "I submitted your sentence to the overlords! Please wait for approval.",
+            ephemeral=True,
+        )
+        self.stop()
 
 bot.run('')
